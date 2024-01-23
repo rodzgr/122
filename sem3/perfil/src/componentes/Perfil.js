12d57e4875b335abad5edb32fda391c4efdecc86
rodzgr <157426464+rodzgr@users.noreply.github.com>
2024-01-23 04:20:51 +0000

    ok

---
+++ sem3/perfil/src/componentes/Perfil.js
@@ -0,0 +1,24 @@
+import React from "react";
+function Perfil(){
+    return(
+        <div class="perfil">
+
+                <h1> rodrigo zanca</h1>
+        
+                <div class="perfil">
+                <img src="/assets/images/perfil_picture.jpeg" alt="" class="perfil">
+                </div>
+                
+                <p> software enginer</p>
+                <p> software enginer</p>
+                <img src="assets/icons/github.svg" alt=""  >
+                <img src="assets/icons/twitter.svg" alt="">
+                    <img src="assets/icons/linkedin.svg" alt="">
+                <img src="assets/icons/youtube.svg" alt="">
+    </div>
+        </div>
+  
+
+    );
+
+}
